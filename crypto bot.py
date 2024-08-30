54c31288ea8ea807e58a8b463ff8de57a2f8fd02
Kokimail <112343609+Kokimail@users.noreply.github.com>
2024-08-29 17:37:14 -0700

    Update crypto bot.py

--- crypto bot.py
+++ crypto bot.py
@@ -5,6 +5,8 @@ from alpaca.data.historical import CryptoHistoricalDataClient
 from alpaca.data.requests import CryptoBarsRequest
 from alpaca.data.timeframe import TimeFrame
 from datetime import datetime, timezone, timedelta
+import pandas as pd
+import time
 
 # setting API properties
 API_KEY = 'PK2I5CCKUGXSDUMF5729'
@@ -18,23 +20,59 @@ data_client = CryptoHistoricalDataClient(API_KEY,API_SECRET)
 # Define the symbol for Bitcoin and the exchange
 symbol = 'BTC/USD'
 
-# Create timezone-aware start and end times
-now = datetime.now(timezone.utc)
-start_time = (now - timedelta(minutes=10)).isoformat()
+# defining bot
 
-# Create a request for the latest minute bar for Bitcoin
-bar_request = CryptoBarsRequest(
-    symbol_or_symbols=symbol,
-    timeframe=TimeFrame.Minute,
-    start= start_time,
-    end = now.isoformat()
-)
+def execute_bot():
 
-# Fetch the bar data
-try:
-    bars = data_client.get_crypto_bars(bar_request)
-    print(bars.df) 
-    print(f"The current price of Bitcoin (BTC) is: ${bars.df.iloc[-1].close}")
+    # Create timezone-aware start and end times
+    m = 30 #set minutes delta
+    mh = m//2 #set halftime
+    now = datetime.now(timezone.utc)
+    start_time_1 = (now - timedelta(minutes=mh)).isoformat() #15 minutes ago
+    start_time_2 = (now - timedelta(minutes=m)).isoformat() #30 minutes ago
 
-except Exception as e:
-    print(f"Error fetching price: {e}")
+    print(now)
+    print(start_time_1)
+    print(start_time_2)
+
+    # Create a request for the latest minute bar for Bitcoin
+    bar_request_1 = CryptoBarsRequest(
+        symbol_or_symbols=symbol,
+        timeframe=TimeFrame.Minute,
+        start= start_time_1,
+        end = now.isoformat()
+    )
+
+    bar_request_2 = CryptoBarsRequest(
+        symbol_or_symbols=symbol,
+        timeframe=TimeFrame.Minute,
+        start= start_time_2,
+        end = start_time_1
+    )
+
+    # Fetch the bar data
+    try:
+        bars_1 = data_client.get_crypto_bars(bar_request_1)
+        bars_2 = data_client.get_crypto_bars(bar_request_2)
+
+        df_1 = pd.DataFrame(bars_1.df)
+        df_2 = pd.DataFrame(bars_2.df)
+        print(df_1)
+        print(df_2)
+        return_1 = (df_1.iloc[-1].close-df_1.iloc[0].close)*100/df_1.iloc[0].close #return for first half of time table
+        return_2 = (df_2.iloc[-1].close-df_2.iloc[0].close)*100/df_2.iloc[0].close #return for second half of time table
+        print(f"The current price of Bitcoin (BTC) is: ${df_1.iloc[-1].close} at {df_1.index.get_level_values('timestamp')[-1]} which is a {round(return_1,2)}% variance from {df_1.index.get_level_values('timestamp')[0]}")
+        print(f"The current price of Bitcoin (BTC) was: ${df_2.iloc[-1].close} at {df_2.index.get_level_values('timestamp')[-1]} which is a {round(return_2,2)}% variance from {df_2.index.get_level_values('timestamp')[0]}")
+        print("Bot executed at", time.ctime())
+    except Exception as e:
+        print(f"Error: {e}")
+
+execute_bot()
+
+# Set up schedule to execute bot
+
+#while True:
+    #execute_bot()
+    #time.sleep(1800) #sleep for 1800 seconds (30 minutes)
+
+# ctrl + c on terminal to break the loop

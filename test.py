0c2646caf2a21b421a1b51a0fed19646686171eb
Kokimail <koki.kawakami@outlook.com>
2024-08-30 17:51:55 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 
+from alpaca.data.timeframe import TimeFrame
 from datetime import datetime, timezone, timedelta
 import pandas as pd
-import time
-    
+from alpaca.data.requests import CryptoBarsRequest
+
+symbol = 'BTC/USD' 
 
 m = 30 #set minutes delta
 mh = m//2 #set halftime
@@ -18,3 +20,21 @@ print(timedelta(minutes=m))
 print(now)
 print(now-timedelta(minutes=mh))
 print(now-timedelta(minutes=m))
+
+    # Create a request for the latest minute bar for Bitcoin
+bar_request_1 = CryptoBarsRequest(
+    symbol_or_symbols=symbol,
+    timeframe=TimeFrame.Minute,
+    start= start_time_1,
+    end = now.isoformat()
+)
+
+bar_request_2 = CryptoBarsRequest(
+    symbol_or_symbols=symbol,
+    timeframe=TimeFrame.Minute,
+    start= start_time_2,
+    end = start_time_1
+)
+
+print(bar_request_1)
+print(bar_request_2)
